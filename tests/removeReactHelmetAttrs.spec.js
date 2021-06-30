c00bb76306e1c93237550188b892dd8fe3c3a641
shoonia <alexanderz@wix.com>
2021-06-30 22:59:59 +0300

    Add tests for removeReactHelmetAttrs

---
+++ tests/removeReactHelmetAttrs.spec.js
@@ -0,0 +1,56 @@
+const { onPreRenderHTML } = require('../gatsby-ssr.js');
+const { Driver } = require('./Driver.js');
+
+const getMock = () => [
+  {
+    type: 'title',
+    key: 'Blog',
+    ref: null,
+    props: {
+      'data-react-helmet': true,
+      children: 'Blog',
+    },
+    _owner: null,
+  },
+];
+
+
+describe('removeReactHelmetAttrs', () => {
+  it('should remove data attrs', () => {
+    const driver = new Driver();
+
+    driver.mockHead(getMock());
+
+    onPreRenderHTML(
+      driver.api,
+      driver.disableAllWith({ removeReactHelmetAttrs: true }),
+    );
+
+    expect(driver.api.replaceHeadComponents).toHaveBeenCalledTimes(1);
+    expect(driver.api.replaceHeadComponents).toHaveBeenCalledWith([
+      {
+        type: 'title',
+        key: 'Blog',
+        ref: null,
+        props: {
+          children: 'Blog',
+        },
+        _owner: null,
+      },
+    ]);
+  });
+
+  it('should not remove data attrs', () => {
+    const driver = new Driver();
+
+    driver.mockHead(getMock());
+
+    onPreRenderHTML(
+      driver.api,
+      driver.disableAllWith(),
+    );
+
+    expect(driver.api.replaceHeadComponents).toHaveBeenCalledTimes(1);
+    expect(driver.api.replaceHeadComponents).toHaveBeenCalledWith(getMock());
+  });
+});
