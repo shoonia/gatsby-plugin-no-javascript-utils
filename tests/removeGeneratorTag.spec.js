19e4c0481a549df5841b4dad44c6821aa6f90a6c
shoonia <alexanderz@wix.com>
2021-06-30 11:20:07 +0300

    Add test

---
+++ tests/removeGeneratorTag.spec.js
@@ -0,0 +1,66 @@
+const { onPreRenderHTML } = require('../gatsby-ssr.js');
+const { Driver } = require('./Driver.js');
+
+const getMock = () => [
+  {
+    type: 'meta',
+    key: 'generator-3.8.1',
+    ref: null,
+    props: {
+      name: 'generator',
+      content: 'Gatsby 3.8.1',
+    },
+    _owner: null,
+  },
+  {
+    type: 'title',
+    key: 'Blog',
+    ref: null,
+    props: {
+      'data-react-helmet': true,
+      children: 'Blog',
+    },
+    _owner: null,
+  },
+];
+
+describe('removeGeneratorTag', () => {
+  it('should remove generator tag', () => {
+    const driver = new Driver();
+
+    driver.mockHead(getMock());
+
+    onPreRenderHTML(
+      driver.api,
+      driver.disableAllWith({ removeGeneratorTag: true }),
+    );
+
+    expect(driver.api.replaceHeadComponents).toHaveBeenCalledTimes(1);
+    expect(driver.api.replaceHeadComponents).toHaveBeenCalledWith([
+      {
+        type: 'title',
+        key: 'Blog',
+        ref: null,
+        props: {
+          'data-react-helmet': true,
+          children: 'Blog',
+        },
+        _owner: null,
+      },
+    ]);
+  });
+
+  it('should not remove generator tag', () => {
+    const driver = new Driver();
+
+    driver.mockHead(getMock());
+
+    onPreRenderHTML(
+      driver.api,
+      driver.disableAllWith(),
+    );
+
+    expect(driver.api.replaceHeadComponents).toHaveBeenCalledTimes(1);
+    expect(driver.api.replaceHeadComponents).toHaveBeenCalledWith(getMock());
+  });
+});
